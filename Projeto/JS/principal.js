a71a3c4e6428615577d5d77004e970fd3bbc2abd
Mizael Monteiro <mizaelmnf@gmail.com>
2024-12-19 13:41:55 -0300

    Update principal.js

--- Projeto/JS/principal.js
+++ Projeto/JS/principal.js
@@ -1,19 +1,6 @@
-document.addEventListener("DOMContentLoaded", function () {
-      const modalOverlay = document.getElementById("modalOverlay");
-      const closeModal = document.getElementById("closeModal");
+function mostrarBoasVindas() {
+    alert("Bem vindo ao site do nosso projeto integrador ");
+}
 
-      // Show the modal
-      modalOverlay.style.display = "flex";
 
-      // Close the modal when the button is clicked
-      closeModal.addEventListener("click", function () {
-        modalOverlay.style.display = "none";
-      });
-
-      // Optional: Close the modal when clicking outside the content
-      modalOverlay.addEventListener("click", function (event) {
-        if (event.target === modalOverlay) {
-          modalOverlay.style.display = "none";
-        }
-      });
-    });
+window.onload = mostrarBoasVindas;
